b65f2c22e5e198b21599a509b6d97e938dca8722
ianrowan <i.rowan299@gmail.com>
2023-03-28 16:28:02 -0500

    add tf-idf for keyword filtering

--- smart_transcribe.py
+++ smart_transcribe.py
@@ -1,4 +1,3 @@
-
 import os
 import whisper
 import spacy
@@ -6,16 +5,40 @@ import openai
 import pyaudio
 import numpy as np
 import threading
+import requests
+import time
+from nltk.corpus import reuters
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 openai.api_key = os.getenv("OPENAI_KEY")
-nlp = spacy.load("en_core_sci_lg")
+nlp = spacy.load("en_core_sci_scibert")
+nlp.enable_pipe("ner")
 #nlp.select_pipes(enable="ner")
 #print(nlp.get_pipe("ner").cfg)
 #nlp.get_pipe("ner").cfg["beam_density"] = 0.00005
 
+tfidf = TfidfVectorizer()
+tfidf_matrix = tfidf.fit_transform([reuters.raw()])
+vocab = tfidf.vocabulary_
+
 model = whisper.load_model("base.en")
 curr_end_token = 0
 frame_start_token = 0
+
+keywords = []
+
+def filter_irrelevant(word):
+    global tfidf_matrix
+    global vocab
+    cutoff_threshold = 0.0012
+    print(word)
+    try:
+        score = tfidf_matrix[0, vocab[word.lower()]]
+        print(score)
+        return score > cutoff_threshold
+    except KeyError:
+        print("not skipping")
+        return False
 
 def gpt_keyword_query(keywords, turbo):
     filer = open("definitions.txt", "r")
@@ -24,9 +47,10 @@ def gpt_keyword_query(keywords, turbo):
     #curr_keywords = "None"
     for keyword in keywords:
         filew = open("definitions.txt", "a")
-        if(f"{keyword}:" not in curr_keywords):
+        if(f"{keyword}:" not in curr_keywords and not filter_irrelevant(str(keyword))):
             #print(f"Keywork {keyword} is: ")
             if turbo:
+                '''
                 response = openai.ChatCompletion.create(
                     model='gpt-3.5-turbo',
                     messages=[
@@ -34,7 +58,13 @@ def gpt_keyword_query(keywords, turbo):
                     {"role": "user", "content": f"In 4 sentences or less explain {keyword}"}
                     ]
                 )
-                response = response["choices"][0]["message"]["content"].replace("\n","")
+                response = response["choices"][0]["message"]["content"].replace("\n","")'''
+
+                response = requests.post("http://127.0.0.1:5000/prompt", 
+                                         json={
+                                            "prompt": f"In 4 sentences or less what is {keyword}?"
+                                         }).json()["text"].replace("\n","")
+                
                 #print(response["choices"][0]["message"]["content"])
                 #print("\n")
             else:
@@ -47,8 +77,8 @@ def gpt_keyword_query(keywords, turbo):
                     frequency_penalty=0.5,
                     presence_penalty=0
                     )
-                print(response["choices"][0]["text"].replace("\n",""))
-                print("\n")
+                #print(response["choices"][0]["text"].replace("\n",""))
+                #print("\n")
                 response = response["choices"][0]["text"].replace("\n","")
             filew.write(f"{keyword}: {response}\n")
         else:
@@ -81,19 +111,28 @@ def transcribe(transribe_item, model, new_split):
     doc = nlp(result[curr_end_token:])
     #print("\nKeywords:" + str(doc.ents))
     curr_end_token = len(result)
-
-    #3. for each keyword, check if exists already and look up def if not 
+    #3. for each keyword, check if exists already and look up def if not
     print(doc.ents)
+    #gpt_keyword_query(doc.ents, True)
     t1 = threading.Thread(target=gpt_keyword_query, args=(doc.ents, False,))
     t1.start()
+    #for item in doc.ents:
+    #    keywords.append(item)
 
 def main():
+    file1 = open("transcript.txt", "w")
+    file1.write("")
+    file1.close()
+    file2 = open("definitions.txt", "w")
+    file2.write("")
+    file2.close()
+
     CHUNKSIZE = 1024 # fixed chunk size
     RATE= 16000
     RECORD_SECONDS = 5
     # initialize portaudio
     p = pyaudio.PyAudio()
-
+    
     input_i = 0
     for i in range(p.get_device_count()):
         dev = p.get_device_info_by_index(i)
@@ -108,6 +147,8 @@ def main():
     print("starting")
     frames = []
     sec_count = 0
+    #wordThread = threading.Thread(target=keyword_job)
+    #wordThread.start()
     try:
         while True: 
             #print("recording")
@@ -130,5 +171,13 @@ def main():
         stream.stop_stream()
         stream.close()
         p.terminate()
+
+def keyword_job():
+    while True:
+        global keywords
+        if len(keywords):
+            word = keywords.pop(0)
+            gpt_keyword_query([word], True)
+        time.sleep(1)
 
 main()
